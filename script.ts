61f7b1eea9476b31c25ca2e812247c97bae08458
SpexFX <pimo.pk04@gmail.com>
2026-07-18 15:48:02 +0200

    - added visiual representation - complete visual overhaul

--- script.ts
+++ script.ts
@@ -8,10 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let back = document.getElementById("answer");
     let question = Questions.question1;
     let answer = Questions.answer1;
+    let whereAt = document.getElementById("whereAt");
     // @ts-ignore
     front.textContent = `${question}`
     // @ts-ignore
     back.textContent = `${answer}`
+    // @ts-ignore
+    whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
     return;
 });
 
@@ -24,91 +27,165 @@ const defaultQuestions = {
 }
 let Questions = JSON.parse(localStorage.getItem("questions") || JSON.stringify(defaultQuestions));
 
-let isFlipped = false;
 let currentQuestion = 1;
 let maxQuestions = parseInt(localStorage.getItem("maxQuestions") || "1");
 let isDefaultQuestion = parseInt(localStorage.getItem("isDefaultQuestion") || "1");
 const selectQuestion = () => `Questions.question${currentQuestion}`;
 const selectAnswer = () => `Questions.answer${currentQuestion}`;
 
-// commented out as it is not needed anymore
-
-/* function flipButton(){
-    let paragraph = document.getElementById("questionAnswer");
-    let answer = eval(selectAnswer());
-    let question = eval(selectQuestion());
-
-    if(isFlipped === false){
-        // @ts-ignore
-        paragraph.textContent = `${answer}`
-        isFlipped = true;
-        console.log(isFlipped);
-        return isFlipped;
-    } else if(isFlipped === true){
-        // @ts-ignore
-        paragraph.textContent = `${question}`
-        isFlipped = false;
-        return isFlipped;
-    }
-} */
-
 function nextQuestion(){
     let front = document.getElementById("question");
     let back = document.getElementById("answer");
+    let whereAt = document.getElementById("whereAt");
     let question;
     let answer;
-    if (currentQuestion < maxQuestions){
+    if (currentQuestion < maxQuestions && maxQuestions > 1) {
         currentQuestion = currentQuestion + 1;
         question = eval(selectQuestion());
-        answer = eval(selectAnswer())
+        answer = eval(selectAnswer());
+        const container = document.querySelector(".card");
         // @ts-ignore
-        front.textContent = `${question}`
-        // @ts-ignore
-        back.textContent = `${answer}`
-        console.log("<");
-        return currentQuestion;
-    }else if(currentQuestion === maxQuestions){
+        if(container.classList.contains("animationFlip")){
+            return;
+        } else{
+            // @ts-ignore
+            container.classList.add("animationFlip");
+            // @ts-ignore
+            container.addEventListener("transitionend", (event) =>{
+
+                if(event.propertyName !== "transform") return;
+                // @ts-ignore
+                container.style.transition = "none";
+                // @ts-ignore
+                container.classList.remove("animationFlip");
+                // @ts-ignore
+                container.offsetHeight;
+                // @ts-ignore
+                container.style.transition = "";
+            }, { once: true })
+            // @ts-ignore
+            front.textContent = `${question}`
+            // @ts-ignore
+            back.textContent = `${answer}`
+
+            // @ts-ignore
+            whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
+            return currentQuestion;
+        }
+    }else if(currentQuestion === maxQuestions && maxQuestions > 1) {
         currentQuestion = 1;
         question = eval(selectQuestion());
-        answer = eval(selectAnswer())
+        answer = eval(selectAnswer());
+        const container = document.querySelector(".card");
         // @ts-ignore
-        front.textContent = `${question}`
-        // @ts-ignore
-        back.textContent = `${answer}`
-        console.log("=");
-        return currentQuestion;
+        if(container.classList.contains("animationFlip")){
+            return;
+        } else{
+            // @ts-ignore
+            container.classList.add("animationFlip");
+            // @ts-ignore
+            container.addEventListener("transitionend", (event) =>{
+
+                if(event.propertyName !== "transform") return;
+                // @ts-ignore
+                container.style.transition = "none";
+                // @ts-ignore
+                container.classList.remove("animationFlip");
+                // @ts-ignore
+                container.offsetHeight;
+                // @ts-ignore
+                container.style.transition = "";
+            }, { once: true })
+            // @ts-ignore
+            front.textContent = `${question}`
+            // @ts-ignore
+            back.textContent = `${answer}`
+            // @ts-ignore
+            whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
+
+            return currentQuestion;
+        }
     }
 }
 
 function previousQuestion(){
     let front = document.getElementById("question");
     let back = document.getElementById("answer");
+    let whereAt = document.getElementById("whereAt");
     let question;
     let answer;
-    if (currentQuestion > 1){
+    if (currentQuestion > 1 && maxQuestions > 1) {
         currentQuestion = currentQuestion - 1;
         question = eval(selectQuestion());
         answer = eval(selectAnswer())
+        const container = document.querySelector(".card")
         // @ts-ignore
-        front.textContent = `${question}`
-        // @ts-ignore
-        back.textContent = `${answer}`
-        return currentQuestion;
-    }else if(currentQuestion === 1){
+        if(container.classList.contains("animationFlip")){
+            return;
+        } else {
+            // @ts-ignore
+            container.classList.add("animationFlipBack");
+            // @ts-ignore
+            container.addEventListener("transitionend", (event) => {
+
+                if (event.propertyName !== "transform") return;
+                // @ts-ignore
+                container.style.transition = "none";
+                // @ts-ignore
+                container.classList.remove("animationFlipBack");
+                // @ts-ignore
+                container.offsetHeight;
+                // @ts-ignore
+                container.style.transition = "";
+            }, {once: true})
+            // @ts-ignore
+            front.textContent = `${question}`
+            // @ts-ignore
+            back.textContent = `${answer}`
+            // @ts-ignore
+            whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
+            return currentQuestion;
+        }
+    }else if(currentQuestion === 1 && maxQuestions > 1) {
         currentQuestion = maxQuestions.valueOf();
         question = eval(selectQuestion());
         answer = eval(selectAnswer())
+        const container = document.querySelector(".card")
         // @ts-ignore
-        front.textContent = `${question}`
-        // @ts-ignore
-        back.textContent = `${answer}`
-        return currentQuestion;
+        if(container.classList.contains("animationFlip")){
+            return;
+        } else {
+            // @ts-ignore
+            container.classList.add("animationFlipBack");
+            // @ts-ignore
+            container.addEventListener("transitionend", (event) => {
+
+                if (event.propertyName !== "transform") return;
+                // @ts-ignore
+                container.style.transition = "none";
+                // @ts-ignore
+                container.classList.remove("animationFlipBack");
+                // @ts-ignore
+                container.offsetHeight;
+                // @ts-ignore
+                container.style.transition = "";
+            }, {once: true})
+            // @ts-ignore
+            front.textContent = `${question}`
+            // @ts-ignore
+            back.textContent = `${answer}`
+            // @ts-ignore
+            whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
+            return currentQuestion;
+        }
     }
 }
 
 function addQuestion(){
     let questionInput = document.getElementById("newCardQuestion") as HTMLInputElement;
     let answerInput = document.getElementById("newCardAnswer") as HTMLInputElement;
+    let whereAt = document.getElementById("whereAt");
+
 
     const form = document.getElementById("newCard");
     // @ts-ignore
@@ -136,6 +213,8 @@ function addQuestion(){
         localStorage.setItem("questions", JSON.stringify(Questions));
         maxQuestions += 1;
         localStorage.setItem("maxQuestions", maxQuestions.toString());
+        // @ts-ignore
+        whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
         return Questions;
     }else{
         alert("Please fill out the WHOLE form!");
@@ -146,13 +225,17 @@ function addQuestion(){
 function resetCards(){
     let front = document.getElementById("question");
     let back = document.getElementById("answer");
+    let whereAt = document.getElementById("whereAt");
     Questions = defaultQuestions;
     maxQuestions = 1;
     isDefaultQuestion = 1;
+    currentQuestion = 1;
     // @ts-ignore
     front.textContent = `${Questions.question1}`;
     // @ts-ignore
     back.textContent = `${Questions.answer1}`;
+    // @ts-ignore
+    whereAt.textContent = `${currentQuestion} / ${maxQuestions}`;
     localStorage.setItem("isDefaultQuestion", isDefaultQuestion.toString());
     localStorage.setItem("maxQuestions", JSON.stringify(maxQuestions));
     localStorage.setItem("questions", JSON.stringify(Questions));
